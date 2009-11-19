f82e5304f9917d9f3f9aac18023a635f7cdf248d
Tadashi MATSUO <tad@mymail.twin.jp>
2009-11-20 01:35:07 +0900

    'twittering-notify-successful-http-get' is introduced. It determines whether every successful GET will be notified or not.

--- twittering-mode.el
+++ twittering-mode.el
@@ -133,6 +133,8 @@ tweets received when this hook is run.")
 ;; %t - text
 ;; %% - %
 
+(defvar twittering-notify-successful-http-get t)
+
 (defvar twittering-buffer "*twittering*")
 (defun twittering-buffer ()
   (twittering-get-or-generate-buffer twittering-buffer))
@@ -617,7 +619,8 @@ directory. You should change through function'twittering-icon-mode'")
 		     noninteractive)
 		(run-hooks 'twittering-new-tweets-hook))
 	    (twittering-render-timeline)
-	    (message (if suc-msg suc-msg "Success: Get.")))
+	    (when twittering-notify-successful-http-get
+	      (message (if suc-msg suc-msg "Success: Get."))))
 	   (t (message status))))
       (message "Failure: Bad http response.")))
   )

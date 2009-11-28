4c8f380af0b6e742df8f240b7374f957f6dfca4b
Yuto Hayamizu <y.hayamizu@gmail.com>
2009-11-29 00:13:56 +0900

    defined some testcases

--- test/test-twittering-mode.el
+++ test/test-twittering-mode.el
@@ -1,26 +1,55 @@
 
-(defsuite twittering-mode nil)
+;; (deftest case-string twittering-mode
+;;   (assert
+;;    (case-string "Rinko"
+;;      (("Rinko") t)
+;;      (t nil)))
+;;   (assert
+;;    (case-string "Rinko"
+;;      (("Manaka") nil)
+;;      (("Nene" "Rinko") t)
+;;      (t nil)))
+;;   (assert
+;;    (case-string "Nene"
+;;      (("Manaka" "Rinko") nil)
+;;      (t t)))
+;;   )
 
-(deftest case-string twittering-mode
-  (assert
-   (case-string "Rinko"
-     (("Rinko") t)
-     (t nil)))
-  (assert
-   (case-string "Rinko"
-     (("Manaka") nil)
-     (("Nene" "Rinko") t)
-     (t nil)))
-  (assert
-   (case-string "Nene"
-     (("Manaka" "Rinko") nil)
-     (t t)))
+(defcase percent-encode nil nil
+  (test-assert-string-equal "Rinko"
+    (twittering-percent-encode "Rinko"))
+  
+  (test-assert-string-equal "%25"
+    (twittering-percent-encode "%"))
+  
+  (test-assert-string-equal "love+plus"
+    (twittering-percent-encode "love plus")))
+
+(defcase case-string nil nil
+  (test-assert-string-equal "Kobayakawa"
+    (case-string "Rinko"
+      (("Rinko") "Kobayakawa")
+      (t "unknown")))
+
+  (test-assert-string-equal "unknown"
+    (case-string "Manaka"
+      (("Rinko") "Kobayakawa")
+      (t "unknown")))
+  
+  (test-assert-string-equal "Kobayakawa"
+    (case-string "Rinko"
+      (("Manaka") "Takane")
+      (("Rinko") "Kobayakawa")
+      (("Nene") "Anegasaki")
+      (t nil)))
+
+  (test-assert-string-equal "Amphibian"
+    (case-string "Rinko"
+      (("Rabbit") "Mammal")
+      (("Salamandar" "Frog") "Amphibian")
+      (t nil)))
   )
 
-(deftest percent-encode twittering-mode
-  (assert-equal "Rinko"
-    (twittering-percent-encode "Rinko"))
-  (assert-equal "%25"
-    (twittering-percent-encode "%"))
-  (assert-equal "love+plus"
-    (twittering-percent-encode "love plus")))
+(defcase format-string nil nil
+  (test-assert-string-equal "foo"
+    (twittering-format-string "" "" nil)))

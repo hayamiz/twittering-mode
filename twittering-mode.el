3a1d49d28874cbc2bfa78747ea9aae2a82a63c8c
Yuto Hayamizu <y.hayamizu@gmail.com>
2009-12-15 04:15:54 +0900

    disable tinyurl when mm-url doesn't exist

--- twittering-mode.el
+++ twittering-mode.el
@@ -45,6 +45,9 @@
 (require 'cl)
 (require 'xml)
 (require 'parse-time)
+(when (< emacs-major-version 22)
+  (require 'un-define)
+  (set-terminal-coding-system 'utf-8))
 
 (defconst twittering-mode-version "0.9")
 (defconst twittering-max-number-of-tweets-on-retrieval 200
@@ -1512,6 +1515,7 @@ If STATUS-DATUM is already in DATA-VAR, return nil. If not, return t."
 (defun twittering-tinyurl-replace-at-point ()
   "Replace the url at point with a tiny version."
   (interactive)
+  (when (featurep 'mm-url)
   (let ((url-bounds (bounds-of-thing-at-point 'url)))
     (when url-bounds
       (let ((url (twittering-tinyurl-get (thing-at-point 'url))))
@@ -1519,7 +1523,7 @@ If STATUS-DATUM is already in DATA-VAR, return nil. If not, return t."
 	  (save-restriction
 	    (narrow-to-region (car url-bounds) (cdr url-bounds))
 	    (delete-region (point-min) (point-max))
-	    (insert url)))))))
+	    (insert url))))))))
 
 ;;;
 ;;; Commands

7f040f277da4d0ec0ad297ebe6e0df0d6652523c
Yuto Hayamizu <y.hayamizu@gmail.com>
2009-12-14 12:36:01 +0900

    add famfamfam logo

--- doc/web/publish.rb
+++ doc/web/publish.rb
@@ -43,6 +43,8 @@ $html_footer = "
 <div id=\"bottom\"> </div>
 <div id=\"footer\">
 Designed by <a href=\"http://www.free-css-templates.com/\">Free CSS Templates</a>, Thanks to <a href=\"http://www.legalhelpers.com/chapter-13-bankruptcy/chapter13.html\">Chapter 13 Bankruptcy</a>
+<br />
+<a href=\"http://www.famfamfam.com/\"><img src=\"images/famfamfam-logo.png\" /></a>
 </div>
 </div>
 

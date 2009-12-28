f5e0a356fafdf42fa5519bd78fc795e0a6fe2137
Yuto Hayamizu <y.hayamizu@gmail.com>
2009-12-29 00:13:04 +0900

    cancel tinyurl test if 'url' is not available

--- test/test-twittering-mode.el
+++ test/test-twittering-mode.el
@@ -93,10 +93,11 @@
   (test-assert-string-equal "love+plus"
     (twittering-percent-encode "love plus")))
 
-(defcase tinyurl nil nil
-  (test-assert-string-equal "http://tinyurl.com/3xsrg5"
-    (twittering-tinyurl-get "http://example.com/example"))
-  )
+(when (require 'url nil t)
+  (defcase tinyurl nil nil
+    (test-assert-string-equal "http://tinyurl.com/3xsrg5"
+      (twittering-tinyurl-get "http://example.com/example"))
+    ))
 
 (defcase case-string nil nil
   (test-assert-string-equal "Kobayakawa"

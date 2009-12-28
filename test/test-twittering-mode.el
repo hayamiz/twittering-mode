0205a8b7e155c41f3a458b8e12a3780d026ac59d
Yuto Hayamizu <y.hayamizu@gmail.com>
2009-12-28 23:53:01 +0900

    add tests of format-status

--- test/test-twittering-mode.el
+++ test/test-twittering-mode.el
@@ -145,6 +145,111 @@
 			      '(("%" . "\n"))))
   )
 
+(lexical-let ((status (car (get-fixture 'timeline-data))))
+  (defcase test-format-status nil nil
+    (test-assert-string-equal "hello world"
+      (twittering-format-status status "hello world"))
+    (test-assert-string-equal "%"
+      (twittering-format-status status "%%"))
+
+
+
+    (test-assert-string-equal "something like emacs"
+      (twittering-format-status status "something like %s"))
+
+    (test-assert-string-equal "We love emacs!"
+      (twittering-format-status status "We love %S!"))
+
+    (setq twittering-icon-mode nil)
+    (test-assert-string-equal ""
+      (twittering-format-status status "%i"))
+    (setq twittering-icon-mode t)
+    (test-assert-ok
+	(string-match "\\s-+" (twittering-format-status status "%i")))
+
+    (test-assert-string-equal
+	"Emacs is the extensible self-documenting text editor."
+      (twittering-format-status status "%d"))
+
+    (test-assert-string-equal "GNU project"
+      (twittering-format-status status "%l"))
+    (test-assert-string-equal " [GNU project]"
+      (twittering-format-status status "%L"))
+    (setcdr (assoc 'user-location status) "")
+    (test-assert-string-equal ""
+      (twittering-format-status status "%l"))
+    (test-assert-string-equal ""
+      (twittering-format-status status "%L"))
+
+    (setcdr (assoc 'in-reply-to-screen-name status) "hoge")
+    (setcdr (assoc 'in-reply-to-status-id status) "123456")
+    (test-assert-string-equal " in reply to hoge"
+      (twittering-format-status status "%r"))
+    (setcdr (assoc 'in-reply-to-screen-name status) "foo")
+    (setcdr (assoc 'in-reply-to-status-id status) "")
+    (test-assert-string-equal ""
+      (twittering-format-status status "%r"))
+    (setcdr (assoc 'in-reply-to-screen-name status) "")
+    (setcdr (assoc 'in-reply-to-status-id status) "654321")
+    (test-assert-string-equal ""
+      (twittering-format-status status "%r"))
+    (setcdr (assoc 'in-reply-to-screen-name status) "")
+    (setcdr (assoc 'in-reply-to-status-id status) "")
+    (test-assert-string-equal ""
+      (twittering-format-status status "%r"))
+
+    (test-assert-string-equal "http://www.gnu.org/software/emacs/"
+      (twittering-format-status status "%u"))
+
+    (test-assert-string-equal "9492852"
+      (twittering-format-status status "%j"))
+
+    (test-assert-string-equal ""
+      (twittering-format-status status "%p"))
+    (setcdr (assoc 'user-protected status) "true")
+    (test-assert-string-equal "[x]"
+      (twittering-format-status status "%p"))
+    (setcdr (assoc 'user-protected status) "false")
+    (test-assert-string-equal ""
+      (twittering-format-status status "%p"))
+
+    (test-assert-string-equal "created at Wed Dec 09 00:44:57 +0000 2009"
+      (twittering-format-status status "created at %c"))
+
+    (test-assert-string-equal "created at 2009/12/09 09:44:57"
+      (twittering-format-status status "created at %C{%Y/%m/%d %H:%M:%S}"))
+
+    ;; (test-assert-string-equal "09:44 午前 12月 09, 2009"
+    ;;   (twittering-format-status status "%@"))
+
+    (test-assert-string-equal "Help protect and support Free Software and the GNU Project by joining the Free Software Foundation! http://www.fsf.org/join?referrer=7019"
+      (twittering-format-status status "%t"))
+
+    (setcdr (assoc 'truncated status) "false")
+    (test-assert-string-equal ""
+      (twittering-format-status status "%'"))
+    (setcdr (assoc 'truncated status) "true")
+    (test-assert-string-equal "..."
+      (twittering-format-status status "%'"))
+    (setcdr (assoc 'truncated status) "false")
+
+    (test-assert-string-equal "web"
+      (twittering-format-status status "%f"))
+
+    (test-assert-string-equal "6480639448"
+      (twittering-format-status status "%#"))
+
+    (setq twittering-icon-mode nil)
+    (test-assert-string-equal " emacs,  09:44 午前 12月 09, 2009:
+  Help protect and support Free Software and the GNU Project by joining the Free Software Foundation! http://www.fsf.org/join?referrer=7019 // from web"
+      (twittering-format-status status "%i %s,  %@:\n  %t // from %f%L%r"))
+    (setq twittering-icon-mode t)
+    (test-assert-string-equal "
+   emacs,  09:44 午前 12月 09, 2009:
+  Help protect and support Free Software and the GNU Project by joining the Free Software Foundation! http://www.fsf.org/join?referrer=7019 // from web"
+      (twittering-format-status status "%i %s,  %@:\n  %t // from %f%L%r"))
+    ))
+
 (defcase test-find-curl-program nil nil
   (test-assert-string-match "curl" (twittering-find-curl-program))
   (with-temp-buffer

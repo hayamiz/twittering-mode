a9314590c7b88b3ad579dd5247e584277e76abb5
Tadashi MATSUO <tad@mymail.twin.jp>
2009-12-30 03:03:56 +0900

    'twittering-image-type()' does not leave unused temporary files.

--- twittering-mode.el
+++ twittering-mode.el
@@ -312,14 +312,14 @@ Otherwise, they are retrieved by `url-retrieve'.")
   (if (and (not (assoc file-name twittering-image-type-cache))
 	   (file-exists-p file-name))
       (if (and twittering-convert-fix-size twittering-use-convert)
-	  (let ((tmpfile (concat (make-temp-file "emacstwit" nil) ".png")))
+	  (let ((tmpfile (make-temp-file "emacstwit")))
 	    (let ((coding-system-for-read 'raw-text)
 		  (coding-system-for-write 'binary))
 	      (call-process twittering-convert-program nil nil nil
 			    file-name "-resize"
 			    (format "%dx%d" twittering-convert-fix-size
 				    twittering-convert-fix-size)
-			    tmpfile)
+			    (concat "png:" tmpfile))
 	      (rename-file tmpfile file-name t))
 	    (add-to-list 'twittering-image-type-cache `(,file-name . png)))
       (let* ((file-output (shell-command-to-string (concat "file -b " file-name)))

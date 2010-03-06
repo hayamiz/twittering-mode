e9c84a02c6371ce7b35c0da83ff202f60b519a02
Satoshi Yatagawa <yata_github@y.hauN.org>
2010-03-06 20:17:57 +0900

    (twittering-http-get-default-sentinel): add an additional information which is related to obsolete public_timeline.

--- twittering-mode.el
+++ twittering-mode.el
@@ -1778,7 +1778,12 @@ Available keywords:
 	      (twittering-render-timeline t new-statuses))
 	    (twittering-add-timeline-history)))
 	(if twittering-notify-successful-http-get
-	    (if suc-msg suc-msg "Success: Get.")
+	    ;;(if suc-msg suc-msg "Success: Get.")
+	    (if suc-msg
+		suc-msg
+	      (if (eq 'public (car spec))
+		  "Success: Get (public_timeline will be obsoleted on 2010-04-05)"
+		"Success: Get."))
 	  nil)))
      (t
       (let ((error-mes (twittering-get-error-message (process-buffer proc))))

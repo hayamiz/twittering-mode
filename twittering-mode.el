95c47b40429cade3f0630745fbd016450dc0241b
Tadashi MATSUO <tad@mymail.twin.jp>
2010-11-13 17:34:52 +0900

    Add new connection method `urllib-http' and `urllib-https'.
    * twittering-mode.el: Add new connection method `urllib-http' and
    `urllib-https'.
    (twittering-connection-type-order): add `urllib-http' and
    `urllib-https'.
    (twittering-connection-type-table): add new entries for
    `urllib-http' and `urllib-https'.
    (twittering-send-http-request): add special treatment for the
    connection method using the url library.
    (twittering-start-http-session-urllib-p): new function.
    (twittering-start-http-session-urllib-https-p): new function.
    (twittering-send-http-request-urllib): new function.
    (twittering-pre-process-buffer-urllib): new function.

--- twittering-mode.el
+++ twittering-mode.el
@@ -331,7 +331,8 @@ Also see `tls-program'.
 If nil, this is initialized with a list of valied entries extracted from
 `tls-program'.")
 
-(defvar twittering-connection-type-order '(curl wget native))
+(defvar twittering-connection-type-order
+  '(curl wget urllib-http native urllib-https))
   "*A list of connection methods in the preferred order."
 
 (defvar twittering-connection-type-table
@@ -346,7 +347,19 @@ If nil, this is initialized with a list of valied entries extracted from
     (wget (check . twittering-start-http-session-wget-p)
 	  (https . t)
 	  (send-http-request . twittering-send-http-request-wget)
-	  (pre-process-buffer . twittering-pre-process-buffer-wget)))
+	  (pre-process-buffer . twittering-pre-process-buffer-wget))
+    (urllib-http
+     (display-name . "urllib")
+     (check . twittering-start-http-session-urllib-p)
+     (https . nil)
+     (send-http-request . twittering-send-http-request-urllib)
+     (pre-process-buffer . twittering-pre-process-buffer-urllib))
+    (urllib-https
+     (display-name . "urllib")
+     (check . twittering-start-http-session-urllib-p)
+     (https . twittering-start-http-session-urllib-https-p)
+     (send-http-request . twittering-send-http-request-urllib)
+     (pre-process-buffer . twittering-pre-process-buffer-urllib)))
   "A list of alist of connection methods.")
 
 (defvar twittering-format-status-function-source ""
@@ -1240,9 +1253,16 @@ The method to perform the request is determined from
     (twittering-send-http-request-internal
      request additional-info
      (lambda (proc status-str connection-info)
-       (let ((status (process-status proc))
+       (let ((status (cond
+		      ((string= status-str "urllib-finished") 'exit)
+		      ((processp proc) (process-status proc))
+		      (t nil)))
 	     (buffer (process-buffer proc))
-	     (exit-status (process-exit-status proc))
+	     (exit-status (cond
+			   ((string= status-str "urllib-finished") 0)
+			   ((processp proc) (process-exit-status proc))
+			   (t 1)))
+	     (command (process-command proc))
 	     (pre-process-func
 	      (cdr (assq 'pre-process-buffer connection-info)))
 	     (mes nil))
@@ -1255,11 +1275,11 @@ The method to perform the request is determined from
 			 (not (memq status '(exit signal closed failed))))
 		     ;; If the process is running, FUNC is not called.
 		     nil)
-		    ((and (process-command proc)
+		    ((and command
 			  (not (= 0 exit-status)))
 		     ;; If the process abnormally exited,
 		     (format "Failure: %s exited abnormally (exit-status=%s)."
-			     (car (process-command proc)) exit-status))
+			     (car command) exit-status))
 		    ((not (buffer-live-p buffer))
 		     (format "Failure: the buffer for %s is already killed."
 			     proc))
@@ -1646,6 +1666,128 @@ The method to perform the request is determined from
 				    (point-min) t)
 	(replace-match "" nil t))
       )))
+
+;;;;
+;;;; Basic HTTP functions with url library
+;;;;
+
+(defun twittering-start-http-session-urllib-p ()
+  "Return t if url library is available, otherwise nil."
+  (require 'url nil t))
+
+(defun twittering-start-http-session-urllib-https-p ()
+  "Return t if url library can be used for HTTPS, otherwise nil."
+  (and (not twittering-proxy-use)
+       (require 'url nil t)
+       (cond
+	((<= 22 emacs-major-version)
+	 ;; On Emacs22 and later, `url' requires `tls'.
+	 (twittering-start-http-session-native-tls-p))
+	((require 'ssl nil t)
+	 ;; On Emacs21, `url' requires `ssl'.
+	 t)
+	((or (fboundp 'open-ssl-stream)
+	     ;; On Emacs21, `url' requires `ssl' in order to use
+	     ;; `open-ssl-stream', which is included in `ssl.el'.
+	     ;; Even if `ssl' cannot be loaded, `open-tls-stream' can be
+	     ;; used as an alternative of the function.
+	     (and (twittering-start-http-session-native-tls-p)
+		  (defalias 'open-ssl-stream 'open-tls-stream)))
+	 (provide 'ssl)
+	 t)
+	(t
+	 nil))))
+
+(defun twittering-send-http-request-urllib (name buffer connection-info sentinel)
+  (let* ((request (cdr (assq 'request connection-info)))
+	 (method (cdr (assq 'method request)))
+	 (scheme (cdr (assq 'scheme request)))
+	 (uri (cdr (assq 'uri request)))
+	 (header-list (cdr (assq 'header-list request)))
+	 (post-body (cdr (assq 'post-body request)))
+	 (use-proxy (cdr (assq 'use-proxy connection-info)))
+	 (proxy-server (cdr (assq 'proxy-server connection-info)))
+	 (proxy-port (cdr (assq 'proxy-port connection-info)))
+	 (coding-system-for-read 'binary)
+	 (coding-system-for-write 'binary)
+	 (url-proxy-services
+	  (when use-proxy
+	    `((,scheme . ,(format "%s:%s" proxy-server proxy-port)))))
+	 (url-request-method method)
+	 (url-request-extra-headers
+	  ;; Remove some headers that should be configured by url library.
+	  ;; They may break redirections by url library because
+	  ;; `url-request-extra-headers' overwrites the new headers
+	  ;; that are adapted to redirected connection.
+	  (apply 'append
+		 (mapcar (lambda (pair)
+			   (if (member (car pair)
+				       '("Host" "Content-Length"))
+			       nil
+			     `(,pair)))
+			 header-list)))
+	 (url-request-data post-body)
+	 (url-show-status twittering-url-show-status)
+	 (tls-program twittering-tls-program)
+	 (coding-system-for-read 'binary)
+	 (coding-system-for-write 'binary))
+    (lexical-let ((sentinel sentinel)
+		  (buffer buffer))
+      (let ((result-buffer
+	     (url-retrieve
+	      uri
+	      (lambda (&rest args)
+		(let ((proc url-http-process)
+		      (url-buffer (current-buffer))
+		      (status-str "urllib-finished"))
+		  ;; Callback may be called multiple times.
+		  ;; (as filter and sentinel?)
+		  (unless (local-variable-if-set-p 'twittering-retrieved)
+		    (set (make-local-variable 'twittering-retrieved)
+			 'not-completed)
+		    (with-current-buffer buffer
+		      (insert-buffer-substring url-buffer))
+		    (set-process-buffer proc buffer)
+		    (unwind-protect
+			(apply sentinel proc status-str nil)
+		      (set-process-buffer proc url-buffer)
+		      (if (eq twittering-retrieved 'exited)
+			  (url-mark-buffer-as-dead url-buffer)
+			(setq twittering-retrieved 'completed))))
+		  (when (memq (process-status proc)
+			      '(nil closed exit failed signal))
+		    ;; Mark `url-buffer' as dead when the process exited
+		    ;; and `sentinel' is completed.
+		    ;; If this `lambda' is evaluated via a filter, the
+		    ;; process may exit before it is finished to evaluate
+		    ;; `(apply sentinel ...)'. In the case, `buffer' should
+		    ;; not be killed. It should be killed after the
+		    ;; evaluation of `sentinel'.
+		    (if (eq twittering-retrieved 'completed)
+			(url-mark-buffer-as-dead url-buffer)
+		      (setq twittering-retrieved 'exited))))))))
+	(when (buffer-live-p result-buffer)
+	  (with-current-buffer result-buffer
+	    (set (make-local-variable 'url-show-status)
+		 twittering-url-show-status))
+	  (get-buffer-process result-buffer))))))
+
+(defun twittering-pre-process-buffer-urllib (proc buffer connection-info)
+  (with-current-buffer buffer
+    (save-excursion
+      (goto-char (point-max))
+      (cond
+       ((search-backward-regexp
+	 "- Peer has closed the GNUTLS connection\r?\n\\'"
+	 nil t)
+	(let ((beg (match-beginning 0))
+	      (end (match-end 0)))
+	  (delete-region beg end)))
+       ((search-backward-regexp "closed\r?\n\\'" nil t)
+	(let ((beg (match-beginning 0))
+	      (end (match-end 0)))
+	  (delete-region beg end)))
+       (t nil)))))
 
 ;;;;
 ;;;; HTTP functions for twitter-like serivce
